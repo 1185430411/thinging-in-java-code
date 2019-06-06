cc28ed583f59929dfbf0ceea0f7c37cf0932cdae
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-06-06 22:31:01 +0800

    Create pattern.java

---
+++ 第九章 接口/pattern.java
@@ -0,0 +1,36 @@
+package night9;
+
+class father{
+	Object way(Object obj) {
+		return  obj;
+	}
+}
+
+class low extends father{
+	String way(Object s) {              //参数也必须相同，否则直接调用父类的way方法
+		return ((String)s).toLowerCase();
+	}
+}
+
+class up extends father{
+	String way(Object s) {
+		return ((String)s).toUpperCase();
+		}
+}
+
+
+public class pattern {
+	public static void way(father f,Object obj) {             //通过不同方法来调不同的方法
+		System.out.println(f.way(obj));
+	}
+	public static void main(String[] args) {
+		way(new low(),"abc");
+		way(new up(),"abc");
+	}
+}
+
+
+/*
+abc
+ABC
+*/
