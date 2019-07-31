c2a04b6c8b868971162cc5417d97a1512685b1b7
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-07-31 15:09:09 +0800

    Create Random类.java

---
+++ 常用类./Random类.java
@@ -0,0 +1,15 @@
+package learn;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Random;
+
+/*
+ * */
+public class learn{
+	public static void main(String[] args) {
+		Random r=new Random();
+		System.out.println(r.nextInt(101));//生成一个0到101，包括0，不包括101的随机数
+		
+	}
+}
