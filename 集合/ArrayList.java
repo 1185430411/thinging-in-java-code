d6607aff4f89a90e4b2b68b4ff060a889db7100f
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-07-31 16:01:11 +0800

    Create ArrayList.java

---
+++ 集合/ArrayList.java
@@ -0,0 +1,29 @@
+package learn;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Random;
+
+/*
+ * */
+public class learn{
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		ArrayList a=new ArrayList();
+		a.add(5);
+		a.add(4);
+		a.add("a");
+		System.out.println(a);  //[5, 4, a] 
+		
+		a.remove("a");
+		System.out.println(a); //[5, 4]
+		
+		System.out.println(a.contains(5));  //true
+		
+		System.out.println(a.isEmpty());  //false
+		
+		a.clear();
+		System.out.println(a.isEmpty());  //true
+	}
+}
