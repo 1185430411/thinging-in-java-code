b64cc92b07b9ae2889e4448398bf3c8d0d9629de
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-10 20:57:56 +0800

    Create 03forward标签.java

---
+++ JSP/03forward标签.java
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<%
+		request.setAttribute("name", "JAY");
+	%>
+	<jsp:forward page="/next.jsp"></jsp:forward>
+	<%--50并没有显示 --%>
+	<%=50 %>
+</body>
+</html>
+
+
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<%=request.getAttribute("name") %>
+</body>
+</html>
