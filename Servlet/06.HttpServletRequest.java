c4421749ab4f403ea3db1536094da281201fca2c
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-10 13:58:16 +0800

    Create 06.HttpServletRequest.java

---
+++ Servlet/06.HttpServletRequest.java
@@ -0,0 +1,48 @@
+package com.jay.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class request
+ */
+public class request extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public request() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    /*获取在html界面上输入的内容*/
+		String name=request.getParameter("username");
+		String password =request.getParameter("password");
+		
+		String[] hobby=request.getParameterValues("hobby");
+		System.out.println(name);
+		System.out.println(password);
+		
+		for(int i=0;i<hobby.length;i++) {
+			System.out.println(hobby[i]);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
