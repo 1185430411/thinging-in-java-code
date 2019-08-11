ab10a10983ed8b696fbbf32965383724ca3d1dd8
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-11 21:46:32 +0800

    Create 15获取上传的文件.java

---
+++ JSP/15获取上传的文件.java
@@ -0,0 +1,46 @@
+package com.jay.test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class upload
+ */
+public class upload extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public upload() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletInputStream is=request.getInputStream();
+		
+		int temp=0;
+		byte[] b=new byte[1024];
+		while((temp=is.read(b))!=0) {
+			System.out.println(new String(b,0,temp));
+		}
+		System.out.println("hi");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
