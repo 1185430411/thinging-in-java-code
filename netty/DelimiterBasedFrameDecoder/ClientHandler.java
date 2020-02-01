790e4d74c334f9d369a9a69429d1aaa73262d951
JAYICE <49588871+1185430411@users.noreply.github.com>
2020-02-01 16:12:18 +0800

    Create ClientHandler.java

---
+++ netty/DelimiterBasedFrameDecoder/ClientHandler.java
@@ -0,0 +1,46 @@
+package com.jay.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class EchoClient {
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap(); // (1)
+            b.group(workerGroup); // (2)
+            b.channel(NioSocketChannel.class); // (3)
+            b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
+            b.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                    ByteBuf byteBuf= Unpooled.copiedBuffer("$_".getBytes());
+                    //单条数据最大长度为1024，分隔符缓冲对象为byteBuf
+                    ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024,byteBuf));
+                    ch.pipeline().addLast(new StringDecoder());
+                    ch.pipeline().addLast(new EchoClientHandler());
+                }
+            });
+
+            // 启动客户端
+            ChannelFuture f = b.connect("localhost",8080).sync(); // (5)
+
+            // 等待连接关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
