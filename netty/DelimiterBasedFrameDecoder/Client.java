5ad4962019c89dffc5dea3da4eea2afa73ae553c
JAYICE <49588871+1185430411@users.noreply.github.com>
2020-02-01 16:11:59 +0800

    Create Client.java

---
+++ netty/DelimiterBasedFrameDecoder/Client.java
@@ -0,0 +1,31 @@
+package com.jay.netty;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoClientHandler extends ChannelInboundHandlerAdapter {
+
+    static final String msg="hi,welcome to netty $_";
+
+    int count=0;
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for(int i=0;i<10;i++){
+            ctx.writeAndFlush(Unpooled.copiedBuffer(msg.getBytes()));
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("this is "+ ++count +"times receive server:"+msg);
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
