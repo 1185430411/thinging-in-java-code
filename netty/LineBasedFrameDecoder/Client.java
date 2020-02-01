8011dbc0a82a5b4c5f3fa7790e2e431c5e665ec1
JAYICE <49588871+1185430411@users.noreply.github.com>
2020-02-01 16:09:23 +0800

    Create Client.java

---
+++ netty/LineBasedFrameDecoder/Client.java
@@ -0,0 +1,42 @@
+package com.jay.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class TimeClient {
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap(); // (1)
+            b.group(workerGroup); // (2)
+            b.channel(NioSocketChannel.class); // (3)
+            b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
+            b.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                    ch.pipeline().addLast(new StringDecoder());
+                    ch.pipeline().addLast(new TimeClientHandler());
+                }
+            });
+
+            // 启动客户端
+            ChannelFuture f = b.connect("localhost",8080).sync(); // (5)
+
+            // 等待连接关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
